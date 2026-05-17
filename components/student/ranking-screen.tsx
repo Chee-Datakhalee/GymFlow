f3cf61c97c6254bc4c08fb2d68e22fb68f8817b2
Chee-Datakhalee <viniciusvclemente@gmail.com>
2026-05-17 10:39:49 -0300

    Update ranking-screen.tsx

--- components/student/ranking-screen.tsx
+++ components/student/ranking-screen.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react" 
 import { Flame, Crown } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { supabase } from "@/lib/supabase"
